e9097d226883a08c9a144e772f8872f229776784
Sam Robertson <Freechurro1@gmail.com>
2020-02-15 15:31:13 -0700

    added login template, config file

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Sam'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+#def index():
+ #   user = {'username': 'Sam'}
+  #  return render_template('index.html', title='Home', user=user)
