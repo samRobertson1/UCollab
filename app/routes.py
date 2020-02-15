e7a146c0abd1fbe64346bc8bddd11b11309c9363
Sam Robertson <Freechurro1@gmail.com>
2020-02-15 14:33:58 -0700

    broke template

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,4 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Sam'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
