d71611e159ceaddbe4af3759aa9e92b6e01b2354
Sam Robertson <Freechurro1@gmail.com>
2020-02-15 17:49:19 -0700

    hopefully added the sqlalchemy database

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'king-k-rool'
-    MONGO_URI = os.environ.get('MONGO_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
